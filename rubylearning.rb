298e361161db86876d4d06d4e2c22e92b916b4c9
MohammadAli <109208273+MohammadAli440@users.noreply.github.com>
2026-03-05 20:05:46 +0530

    start learning ruby

---
+++ rubylearning.rb
@@ -0,0 +1,7 @@
+# Prints the output without adding a new line at the end.
+print"Hello Wolrd"
+print "Welcome to Ruby programming"
+
+# Prints the output and automatically adds a new line after it.
+puts "My name is Ali"
+puts "I am learning Ruby"
