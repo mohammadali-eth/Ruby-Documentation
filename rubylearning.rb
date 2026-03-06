f3545131233476e364ebfd14a1345de8a687e1fc
MohammadAli <109208273+MohammadAli440@users.noreply.github.com>
2026-03-06 11:11:45 +0530

    variables, data type and strings are done

--- rubylearning.rb
+++ rubylearning.rb
@@ -5,3 +5,34 @@ print "Welcome to Ruby programming"
 # Prints the output and automatically adds a new line after it.
 puts "My name is Ali"
 puts "I am learning Ruby"
+
+# Variables in Ruby
+character_name = "Ali"
+character_age = 25
+
+puts "There once was a  man named #{character_name},"
+puts "he was #{character_age} years old."
+
+# You can change the value of a variable by reassigning it.
+character_name = "Ahmed"
+character_age = 30
+puts "He really liked the name #{character_name},"
+puts "but didn't like being #{character_age}."
+
+# Data types in Ruby
+name = "Ali" # String
+age = 25 # Integer
+height = 5.9 # Float
+is_student = true # Boolean
+nil_value = nil # nil
+
+puts "Name: #{name}"
+puts "Age: #{age}"
+puts "Height: #{height}"
+puts "Is student: #{is_student}"
+puts "Nil value: #{nil_value}"
+
+# Working with strings (most common data type in Ruby)
+puts "Hey there!\" I'm learning Ruby programming." # Using /" to add double quot in single string.
+puts "This is a \n new line." #Using \n to add a new line in single string.
+puts "This is a \t tab space." # Using \t to add a tab space in single string.
